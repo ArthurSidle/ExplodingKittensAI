39c7f9d6b53a2da0a51b9d00d722066186d2911f
Arthur <sidle.arthur@gmail.com>
2023-07-02 14:52:05 +0100

    Tkinter boilerplate

---
+++ gui.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class GUI:
+    def __init__(s):
+        s.root = Tk()
+        s.root.title('Title goes here')
+        s.root.iconbitmap('icon.ico')
+
+        s.root.mainloop()
+
+GUI()
